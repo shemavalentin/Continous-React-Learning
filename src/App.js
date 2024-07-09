081f66582232ceff31ee7d8bd495ad4c918b189b
Shema Valentin <shemavalentin@gmail.com>
2024-07-09 16:01:41 +0200

    Advanced State sharing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState } from "react";
+import Board from "./tic-tak-toe";
 
 export default function MyApp() {
   const [count, setCount] = useState(0);
@@ -9,6 +10,7 @@ export default function MyApp() {
   }
   return (
     <div>
+      <Board />
       <h1>Both buttons chages when one is clicked</h1>
       <MyButton count={count} onClick={handleClick} />
       <br />
